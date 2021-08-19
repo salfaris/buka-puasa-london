edd7b700b6897c867363b0b33b17622bf6a07bc1
Salman <salmanfaris98@gmail.com>
2021-08-19 18:21:04 +0100

    :sparkles: Added csv functionality and restrict print (to current day)

--- script_london.py
+++ script_london.py
@@ -1,18 +1,34 @@
 from sys import argv, exit
+from datetime import datetime
 from prayer_times import get_london_break_fast_time
+import pandas as pd
+
+limit_today = True
+
+def break_fast_time_to_df(csv_path):
+    df = get_london_break_fast_time()
+    df.to_csv(csv_path, index=False)
 
 if __name__ == '__main__':
     
-    limit_today=True
+    csv_path = 'early-maghrib-london.csv'
     
     if len(argv) > 1:    
         if ('-f' in argv) or ('--full' in argv):
-            limit_today=False
+            limit_today = False
+        elif ('--fetch' in argv):
+            break_fast_time_to_df(csv_path)
         else:
             print("Unknown argument used. For full display, use '-f' or '--full'.")
             exit(1)
     
     print("Getting London buka puasa time...")
-    df = get_london_break_fast_time(limit_today=limit_today)
-    print(df)
+    today = datetime.today().day
+    skiprow_ub = today if limit_today else 0
+    try:
+        df = pd.read_csv(csv_path, skiprows=range(1, skiprow_ub))
+    except FileNotFoundError:
+        break_fast_time_to_df(csv_path)
+        df = pd.read_csv(csv_path, skiprows=range(1, skiprow_ub))
     
+    print(df)
